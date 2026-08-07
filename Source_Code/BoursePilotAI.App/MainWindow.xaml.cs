2d92ce3ebcae745a960db6903b2904fed477ed2e
majid10243 <jafarpoya@gmail.com>
2026-08-07 23:22:33 +0330

    Add BoursePilotAI WPF main window code

---
+++ Source_Code/BoursePilotAI.App/MainWindow.xaml.cs
@@ -0,0 +1,12 @@
+using System.Windows;
+
+namespace BoursePilotAI.App;
+
+public partial class MainWindow : Window
+{
+    public MainWindow()
+    {
+        InitializeComponent();
+        Title = "BoursePilotAI";
+    }
+}
