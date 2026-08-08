6689923d0a4ab862b72f106fe2672950c5cc9e2d
majid10243 <jafarpoya@gmail.com>
2026-08-08 12:22:53 +0330

    Add stock item model for dashboard

---
+++ Source_Code/BoursePilotAI.App/StockItem.cs
@@ -0,0 +1,10 @@
+namespace BoursePilotAI
+{
+    public class StockItem
+    {
+        public string Symbol { get; set; } = "";
+        public string Price { get; set; } = "";
+        public string Change { get; set; } = "";
+        public string Status { get; set; } = "";
+    }
+}
